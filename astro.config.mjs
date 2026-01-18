d0c3d21f443b95be43d2053e718ebd79aa251789
Spencer Moore <7570736+Vexlix@users.noreply.github.com>
2026-01-18 16:04:24 -0500

    Config. changes and slight restructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://64bit.io",
+	compressHTML: true,
+	build: {
+		inlineStylesheets: "auto",
+		assets: "_astro",
+	},
+	vite: {
+		build: {
+			cssCodeSplit: true,
+			minify: "esbuild",
+		},
+	},
+});
